d6b175dbdd396eea9156719ff681e981159e8e25
Daniel Watts <daniel.watts@bcgdv.com>
2018-11-05 18:16:47 +0100

    Something working

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 	"time"
+	"sort"
 )
 
 // our main function
@@ -22,12 +23,66 @@ func main() {
 }
 
 func GetLeagueSquadBreakdowns(w http.ResponseWriter, r *http.Request) {
-	var ffApiUri = "https://fantasy.premierleague.com/drf"
 	var leagueId = 592906
-	var url = fmt.Sprintf("%s/leagues-classic-standings/%d?phase=1", ffApiUri, leagueId)
 	league := &League{}
-	getJson(url, league)
-	json.NewEncoder(w).Encode(league)
+
+	var staticData = getStaticFFData()
+	getJson(fmt.Sprintf("%s/leagues-classic-standings/%d?phase=1", ffApiUri, leagueId), league)
+
+	breakdown := &Breakdown{}
+	for i, team := range league.Table.Teams {
+		getJson(fmt.Sprintf("%s/entry/%d/event/11/picks", ffApiUri, team.Id), &league.Table.Teams[i].Squad)
+		
+		for j, squadPlayer := range league.Table.Teams[i].Squad.Players {
+			for _, player := range staticData.Players {
+				if squadPlayer.Id == player.Id {
+					var name = fmt.Sprintf("%s %s", player.FirstName, player.LastName)
+					squadPlayer.Name = name
+					league.Table.Teams[i].Squad.Players[j].Name = name
+					break
+				}
+			}
+
+			playerExists := false
+			for k, player := range breakdown.Players {
+				if player.Id == squadPlayer.Id {
+					playerExists = true
+					breakdown.Players[k].TotalPick += 1
+					if (team.Rank < 6) { breakdown.Players[k].Top5Pick += 1 }
+					if (team.Rank < 11) { breakdown.Players[k].Top10Pick += 1 }
+					break
+				}	
+			}
+
+			if !playerExists {
+				var playerBreakdown = PlayerBreakdown{
+					Id: squadPlayer.Id,
+					Name: squadPlayer.Name,
+					TotalPick: 1,	
+				}			
+
+				if (team.Rank < 6) { playerBreakdown.Top5Pick += 1 }
+				if (team.Rank < 11) { playerBreakdown.Top10Pick += 1 }
+
+				breakdown.Players = append(breakdown.Players, playerBreakdown)
+			}
+		}
+	}
+
+	sort.Slice(breakdown.Players, func(i, j int) bool {
+		return breakdown.Players[i].Top10Pick > breakdown.Players[j].Top10Pick
+	})
+
+	json.NewEncoder(w).Encode(breakdown)
+}
+
+func getStaticFFData() *StaticData {
+	staticData := &StaticData{}
+	err := getJson(fmt.Sprintf("%s/bootstrap-static", ffApiUri), staticData)
+	if err != nil {
+		panic(err.Error())
+	}
+	return staticData
 }
 
 func getJson(url string, target interface{}) error {
@@ -51,16 +106,49 @@ func getJson(url string, target interface{}) error {
     return json.NewDecoder(response.Body).Decode(target)
 }
 
+var ffApiUri = "https://fantasy.premierleague.com/drf"
+
 type League struct {
 	Table Table `json:"standings"`
 }
 
 type Table struct {
-	Team []Team `json:"results"`
+	Teams []Team `json:"results"`
 }
 
 type Team struct {
 	Id         int `json:"entry"`
 	Rank       int `json:"rank"`
 	PlayerName string `json:"player_name"`
+	Squad 	   Squad
 }
+
+type Squad struct {
+	Players []Player `json:"picks"`
+}
+
+type Player struct {
+	Id 		 int `json:"element"`
+	Position int `json:"position"`
+	Name 	 string 
+}
+
+type StaticData struct {
+	Players []struct {
+		Id 		  int `json:"id"`
+		FirstName string `json:"first_name"`
+		LastName  string `json:"second_name"`
+	} `json:"elements"`
+}
+
+type Breakdown struct {
+	Players []PlayerBreakdown `json:"players"`
+}
+
+type PlayerBreakdown struct {
+	Id	 		int `json:"id"`
+	Name 		string `json:"name"`
+	Top5Pick  int `json:"top_5_pick"`
+	Top10Pick int `json:"top_10_pick"`
+	TotalPick int `json:"total_pick"`
+} 
